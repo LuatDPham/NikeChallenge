2e257694843509b59ab79fdc7f1d7a242bcf711b
Luat Pham <luat32@gmail.com>
2020-12-02 16:23:18 -0500

    Added PullRequest class

---
+++ NikeChallenge/Model/PullRequest.swift
@@ -0,0 +1,13 @@
+//
+//  PullRequest.swift
+//  NikeChallenge
+//
+//  Created by Luat Pham on 12/2/20.
+//  Copyright © 2020 Rave BizzDev. All rights reserved.
+//
+
+import Foundation
+
+class TestingPullRequest {
+    
+}
